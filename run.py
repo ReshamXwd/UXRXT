c79b2e81ed4ffd9bdb582e8bbfee7f91b248283e
Resham - Xwd <128343127+ReshamXwd@users.noreply.github.com>
2024-06-21 05:11:29 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 import platform,os
 bit = platform.architecture()[0]
 print(' CHECKING FOR UPDATES');os.system('git pull')
-print(' The tool is in maintenance for few days');exit()
+if bit == '32bit':
+    import UXRXT32
+elif bit == '64bit':
+    import UXRXT64
+else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
